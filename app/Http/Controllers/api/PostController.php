d84007c4c4afda4ef0be04b3e19f8887aed05c32
Ali Hayder <ali.hayder.rzayyig@gmail.com>
2023-01-07 22:15:09 +0300

    add post CROD controller and api route.

---
+++ app/Http/Controllers/api/PostController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+        return $post;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,Post $post)
+    {
+        $post->update($request->all());
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        return $post->delete();
+    }
+
+
+    /**
+     * Search for name.
+     *
+     * @param  String  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        // return $name;
+        return Post::where('title', 'like', '%'.$name.'%')->get();
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route::get('test', function () {
+//     return Post::all();
+// });
+
+// Route::post('test/create', function (Request $request) {
+//     $post = Post::create([
+//         'title' => $request->title,
+//         'body' => $request->body
+//     ]);
+//     return $post;
+// });
+
+// Route::get('test', [PostController::class, 'index']);
+// Route::get('test/{post}', [PostController::class, 'show']);
+// Route::post('test/create', [PostController::class, 'store']);
+
+Route::get('post/{name}/search', [PostController::class, 'search']);
+Route::resource('post', PostController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
